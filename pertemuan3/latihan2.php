823175e5a814d0e4b535a6237d6db6f4e8ad8c3f
AlfiS.R <alfisyahrin673@gmail.com>
2021-02-19 14:55:47 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,28 @@
+<?php
+// Alfi Syahrin Rachmat
+// 203040054
+// https://github.com/Alfisyah672/pw2021_203040054
+// Pertemuan 3, 19 Februari 2021
+// Mempelajari :
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20;
+if($x < 20){
+    echo "benar";
+} else if($x==20){
+    echo "bingo";
+}
+else {
+    echo "salah";
+}
+?>
